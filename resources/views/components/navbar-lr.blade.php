568775dcd8070c9c7defdde44420082244f7e13a
Krishna Ardi <baguskrishna52@gmail.com>
2026-03-19 06:34:27 +0800

    Nambahin navbar buat user login, registrasi, dan sejenisnya

---
+++ resources/views/components/navbar-lr.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <div class=" w-full lg:w-full bg-white flex flex-row lg:flex-row p-6">
+        <img class=" size-5" src="assets/bendera.png" alt="Lambang Garuda">
+        <img class=" size-5" src="assets/polri.png" alt="Logo Polri">
+    </div>
+</header>
+
+    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
