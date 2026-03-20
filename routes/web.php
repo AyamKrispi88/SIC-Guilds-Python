ac97248f885a86de25a6e233d43245e8be8f01d7
Krishna Ardi <baguskrishna52@gmail.com>
2026-03-20 14:18:55 +0800

    Penambahan banyak - Krishna

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@ Route::get('/admin', function() {
     return view('Admin.HomePageAdmin');
 });
 
+Route::get('/user-login', function() {
+    return view('User.login');
+});
+
+Route::get('/confirm', function() {
+    return view('User.inbox-send');
+});
+
+Route::get('/password-changed', function() {
+    return view('User.change-password');
+});
+
+
