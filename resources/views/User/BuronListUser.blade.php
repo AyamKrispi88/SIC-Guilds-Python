40d6119d4ced5c9c15035ceb1e70a4ea71152472
Krishna Ardi <baguskrishna52@gmail.com>
2026-03-20 16:11:44 +0800

    Penambahan banyak - Krishna

---
+++ resources/views/User/BuronListUser.blade.php
@@ -0,0 +1,5 @@
+@extends('User.PakemUser')
+@section('title', 'List Buronan')
+@section('konten')
+    <main></main>
+@endsection
--- resources/views/User/HomePageUser.blade.php
+++ resources/views/User/HomePageUser.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="icon" href="{{ asset('assets/logo1.png') }}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Beranda</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class=" bg-[#E0E0E0]">
