e6c76152b7f4fe29b1a155bf2bb4e47855c2d80e
Krishna Ardi <baguskrishna52@gmail.com>
2026-03-20 23:42:23 +0800

    Nambah font, nambah beberapa juga

--- app/View/Components/navbar-admin.php
+++ app/View/Components/navbar-admin.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class navbar-admin extends Component
+class NavbarAdmin extends Component
 {
     /**
      * Create a new component instance.
