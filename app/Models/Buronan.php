454e5f9fcf8964caba473708423035e200ae9bf7
gungyuda07 <a.yudhadharma@gmail.com>
2026-04-08 23:42:52 +0800

    Revisi models

--- app/Models/Buronan.php
+++ app/Models/Buronan.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Buronan extends Model
 {
     public function laporan_buronan_masuk() {
-    return $this->hasMany(LaporanBuronanMasuk::class, 'buron_id');
+    return $this->hasOne(LaporanBuronanMasuk::class, 'buron_id');
     }
 }
